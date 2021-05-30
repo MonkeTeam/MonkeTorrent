c5afc45705dc1c14f84500c37211d6df7eafc054
Jakaria Blaine <jakariablaine120@gmail.com>
2021-05-30 13:29:27 +0600

    Add testing code

---
+++ src/utils/monke_torrent.js
@@ -0,0 +1,28 @@
+const WebTorrent = require('webtorrent');
+
+class MonkeTorrent
+{
+	constructor()
+	{
+		this.torrent = new WebTorrent();
+	}
+
+	async add(torrent_info, path, callback)
+	{
+		return await this.torrent.add(torrent_info, { path: path }, t => {
+			return t;
+		});
+	}
+
+	seed(path, callback)
+	{
+		this.torrent.seed(path, callback);
+	}
+
+	getProgress(torrent)
+	{
+		return torrent.progress;
+	}
+}
+
+module.exports = MonkeTorrent;
